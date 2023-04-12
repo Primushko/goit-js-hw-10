6686422ed9b9efcbeefbc46e652a054618d00d55
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-04-12 13:49:16 +0300

    fetchCountries.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 function fetchCountries(name) {
-    // const baseUrl = 'https://restcountries.com/v3.1/name';
-    // const url = `${baseUrl}/${name}?fields=name.official,capital,population,flags.svg,languages`;
+
     const baseUrl = `https://restcountries.com/v3.1/all`;
     const url = `${baseUrl}?fields= name.official,capital,population,flags.svg,languages`;
-
     return fetch(url)
     .then(response => {
         if (response.ok) {
